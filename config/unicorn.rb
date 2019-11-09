361c5f183348cecafc71a44648545566363d7816
kt-ozi <9_t0rres_9@ezweb.ne.jp>
2019-11-09 23:55:44 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 # ../が一つ増えている
 app_path = File.expand_path('../../../', __FILE__)
-
+shared_path = File.expand_path('../../../shared/', __FILE__)
 worker_processes 1
 # currentを指定
 working_directory "#{app_path}/current"
